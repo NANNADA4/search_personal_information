0333a7f13d0a921079777c7ff8612364457f33d0
NANNADA4 <coco96062233@icloud.com>
2024-09-03 11:05:51 +0900

    pattern update

--- personalinfo_detector.py
+++ personalinfo_detector.py
@@ -14,16 +14,15 @@ def extract_infos_from_pdf(pdf_file):
 
         pattern_emails = re.findall(
             r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', text)
-        pattern_jumins = re.findall(r'\d{6}-\d{7}', text)
+        pattern_jumins = re.findall(r'\d{2}[01]\d[0123]\d- [1-4]\d{6}', text)
         pattern_credit_num = re.findall(
             r'\b\d{4}-\d{4}-\d{4}-\d{4}\b', text)
         pattern_cellphone_num = re.findall(
             r'\b(010-\d{4}-\d{4}|01[16789]-\d{3,4}-\d{4})\b', text)
         pattern_driver = re.findall(r'\d{2}-\d{2}-\d{6}-\d{2}', text)
         # pattern_passport = re.findall(r'([a-zA-Z]{1}|[a-zA-Z]{2})\d{8}', text)
-        pattern_account = re.findall(
-            r'^(\d{1,})(-(\d{1,})){1,}', text)
-        pattern_health = re.findall(r'[1257][-~.\s][0-9]{10}', text)
+        # pattern_account = re.findall(r'^(\d{1,})(-(\d{1,})){1,}', text)
+        # pattern_health = re.findall(r'[1257][-~.\s][0-9]{10}', text)
         # pattern_foreign = re.findall(r'([01][0-9]{5}[\s~-]+[1-8][0-9]{6}|[2-9][0-9]{5}[\s~-]+[1256][0-9]{6})', text)
 
         for email in pattern_emails:
@@ -44,12 +43,12 @@ def extract_infos_from_pdf(pdf_file):
         # for passport in pattern_passport:
         #     infos.append((os.path.basename(pdf_file),
         #                  page_num + 1, '여권번호', passport))
-        for account in pattern_account:
-            infos.append((os.path.basename(pdf_file),
-                         page_num + 1, '계좌번호', account))
-        for health in pattern_health:
-            infos.append((os.path.basename(pdf_file),
-                         page_num + 1, '건강보험번호', health))
+        # for account in pattern_account:
+        #    infos.append((os.path.basename(pdf_file),
+        #                 page_num + 1, '계좌번호', account))
+        # for health in pattern_health:
+        #    infos.append((os.path.basename(pdf_file),
+        #                 page_num + 1, '건강보험번호', health))
         # for foreign in pattern_foreign:
         #     infos.append((os.path.basename(pdf_file),
         #                  page_num + 1, '외국인등록번호', foreign))
