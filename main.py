dceaa006f7359a3a8b7bf445137449f55d5cb73d
NANNADA4 <coco96062233@icloud.com>
2024-09-04 16:00:14 +0900

    add welcome text

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ main 함수. 폴더를 순회하며 hwp, pdf, xlsx 파일에 한해 개인정보
 from module.processing_folder import processing_folder
 
 if __name__ == "__main__":
+    print("\n>>>>>>개인정보 추출기<<<<<<\n")
+    print("-"*24)
     folder_path = input("작업할 폴더 경로를 입력하세요: ")
     excel_file = input(
         "엑셀파일 경로를 입력하세요(확장자포함. 파일이 존재하지 않을 경우 새로 생성): ")
